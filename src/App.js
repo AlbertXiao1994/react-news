21e6e7dfb8297680dc002eca38febe2913986827
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-03 21:16:21 +0800

    重新配置anr design开发环境

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MediaQuery from "react-responsive";
+import { Button } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div>
         <MediaQuery minDeviceWidth={1224}>
+          <Button type="primary">Button</Button>
         </MediaQuery>
         <MediaQuery maxDeviceWidth={1223}>
         </MediaQuery>
